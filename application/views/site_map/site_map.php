0d4b92d6972d265549df4e3521d44e5cad6c2f68
vishnu <ijkoffers@gmail.com>
2022-07-02 20:05:37 +0530

    Update site_map.php

--- application/views/site_map/site_map.php
+++ application/views/site_map/site_map.php
@@ -18,7 +18,7 @@
 
 
 
-for($k = 0 ; $k < 10 ; $k++){
+for($k = 0 ; $k < 1 ; $k++){
 $this->db->where('uri_string_in_meta !=','0');
 echo 'uri_string_in_meta !=0 -- '. $count2 = $this->db->get('all_india_po_list')->num_rows();
 echo '<br>';
