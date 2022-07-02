0e3517a3d4d6fabe4f3b1320e4ac0c797986f95d
vishnu <ijkoffers@gmail.com>
2022-07-02 17:41:57 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $config['base_url'] = '';
 if($_SERVER['SERVER_ADDR'] == '103.20.212.253'){
-    $config['base_url'] = 'https://ijkoffers.com';
+    $config['base_url'] = 'https://pincodes.ind.in';
     }else{
     $config['base_url'] = 'http://localhost/pincode';
     }
