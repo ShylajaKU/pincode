ae02c9245e042eec09404c06bde8e6e2d3eda86c
vishnu <ijkoffers@gmail.com>
2022-07-02 21:34:50 +0530

    Update site_map.php

--- application/views/site_map/site_map.php
+++ application/views/site_map/site_map.php
@@ -1,15 +1,17 @@
 <?php
 
 
-$this->db->where('meta_desc_len',null);
-$res = $this->db->get('meta')->result_array();
-var_dump($res);
+// $this->db->where('meta_desc_len',null);
+// $res = $this->db->get('meta')->result_array();
+// var_dump($res);
 
-foreach($res as $r){
-    $sl_no = $r['sl_no'];
-    $this->db->where('sl_no',$sl_no);
-    $data = array(
-        'visibility' => 0,
-    );
-    $this->db->update('meta',$data);
-}
+// foreach($res as $r){
+//     $sl_no = $r['sl_no'];
+//     $this->db->where('sl_no',$sl_no);
+//     $data = array(
+//         'visibility' => 0,
+//     );
+//     $this->db->update('meta',$data);
+// }
+
+echo base_url();
