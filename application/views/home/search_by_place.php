8c6f4ec33e49e11f5fb3c1fd785c942e08d4be16
vishnu <ijkoffers@gmail.com>
2022-07-03 16:16:21 +0530

    -

--- application/views/home/search_by_place.php
+++ application/views/home/search_by_place.php
@@ -1,3 +1,7 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css" integrity="sha256-ze/OEYGcFbPRmvCnrSeKbRTtjG4vGLHXgOqsyLFTRjg=" crossorigin="anonymous" />
+
 
   <div class="container cont">
     <?= form_open('state_entered',array('id'=>'form-1',))?>
