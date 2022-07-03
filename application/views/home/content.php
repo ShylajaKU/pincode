40ea59e59cb49ac18ad0067307f395d16a822dfc
vishnu <ijkoffers@gmail.com>
2022-07-03 21:30:25 +0530

    n

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -62,7 +62,7 @@ If a Sub Office also handles mail deliveries then that will have another PIN cod
 </section>
 <br>
 <section>
-    <a href="<?= base_url('list-of-all-pincodes-of-india');?>">View List of Post Offices</a>
+    <a href="<?= base_url('list-of-all-pincodes-in-india');?>">View List of Post Offices</a>
 </section>
 
 </div>
