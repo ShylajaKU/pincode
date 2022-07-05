66094fde9482749159cbf217edd1c9b432883a59
vishnu <ijkoffers@gmail.com>
2022-07-05 15:44:11 +0530

    Update search_result.php

--- application/views/search_result/search_result.php
+++ application/views/search_result/search_result.php
@@ -181,3 +181,15 @@ thumb_down
 }
 </script>
 
+
+<!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4176141279201887" -->
+     <!-- crossorigin="anonymous"></script> -->
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-4176141279201887"
+     data-ad-slot="7856180736"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
