5ac4fdd530ab8db9e9bd674b81b550909fbb0737
vishnu <ijkoffers@gmail.com>
2022-07-06 08:50:48 +0530

    jj

--- application/controllers/Meta_controller.php
+++ application/controllers/Meta_controller.php
@@ -8,6 +8,41 @@ public function site_map_fc(){
     $this->load->view('home/header');
     $this->load->view('site_map/site_map');
 
+    
+// $this->load->library('calendar');
+// echo $this->calendar->generate();
+
+// echo $this->calendar->generate(2006, 6);
+
+// $data = array(
+//     3  => 'http://example.com/news/article/2006/06/03/',
+//     7  => 'http://example.com/news/article/2006/06/07/',
+//     13 => 'http://example.com/news/article/2006/06/13/',
+//     26 => 'http://example.com/news/article/2006/06/26/'
+// );
+
+// echo $this->calendar->generate(2006, 6, $data);
+
+// $prefs = array(
+//     'start_day'    => 'saturday',
+//     'month_type'   => 'long',
+//     'day_type'     => 'short'
+// );
+
+// $this->load->library('calendar', $prefs);
+
+// echo $this->calendar->generate();
+
+// $prefs = array(
+//     'show_next_prev'  => TRUE,
+//     'next_prev_url'   => 'http://example.com/index.php/calendar/show/'
+// );
+
+// $this->load->library('calendar', $prefs);
+
+// echo $this->calendar->generate($this->uri->segment(3), $this->uri->segment(4));
+
+
 
 
     // $this->meta_model->run_meta_for_all_given($uri_string);
--- application/views/site_map/site_map.php
+++ application/views/site_map/site_map.php
@@ -61,28 +61,80 @@
 //     $this->db->update('all_india_po_list',$data);
 // }
 
-// $this->db->where('random_h','0');
-// $query = $this->db->get('all_india_po_list');
-// echo $no = $query->num_rows();
+$this->db->where('random_h','0');
+$query = $this->db->get('all_india_po_list');
+echo $no = $query->num_rows();
 
-// // $run = ceil($no/10000);
-// for($j = 0; $j < 10 ;$j++){
-//     $this->db->limit(1000);
-// $this->db->select('sl_no');
-// $this->db->where('random_h','0');
-// $query = $this->db->get('all_india_po_list');
-// $result = $query->result_array();
-// foreach($result as $res){
-//     $sl_no = $res['sl_no'];
-//     $data = array(
-//         'random_h' => rand(159,1352),
-//     );
-//     $this->db->where('sl_no',$sl_no);
-//     $this->db->update('all_india_po_list',$data);
-// }
+// $run = ceil($no/10000);
+for($j = 0; $j < 10 ;$j++){
+    $this->db->limit(1000);
+$this->db->select('sl_no');
+$this->db->where('random_h','0');
+$query = $this->db->get('all_india_po_list');
+$result = $query->result_array();
+foreach($result as $res){
+    $sl_no = $res['sl_no'];
+    $data = array(
+        'random_h' => rand(59,263),
+    );
+    $this->db->where('sl_no',$sl_no);
+    $this->db->update('all_india_po_list',$data);
+}
 
-// }
+}
 
-// $this->db->where('random_h','0');
-// $query = $this->db->get('all_india_po_list');
-// echo $no = $query->num_rows();
+$this->db->where('random_h','0');
+$query = $this->db->get('all_india_po_list');
+echo $no = $query->num_rows();
+?>
+<!-- <title>Calendar with template</title>
+<style>
+	.shift{
+		margin-left:100px;
+	}
+	
+	table{
+		border:15px solid #FD5196;
+		margin-top:20px;
+		
+		
+	}
+	td{
+		text-align:center;
+		border:2px solid #E6C1EB;
+		font-size:18px;
+		font-weight:bold;
+	}
+	th{
+		background:#FD5196;
+		font-size:20px;
+		color:white;
+	}
+	.prevcell a, .nextcell a{
+		color:white;
+		text-decoration:none;
+	}
+	
+	tr.wk_nm{
+		background:#E6C1EB;
+		color:#AB08BD;
+		font-size:17px;
+		font-weight:bold;
+		width:10px;
+		padding:5px;
+	}
+	
+	.highlight{
+		background:#FD5196;
+		color:white;
+		padding:10px;
+	}
+	
+</style>
+</head>
+<body>
+<h1>Particular month/year calendar</h1>
+
+<p class="shift"><?php echo $this->calendar->generate();?></p>
+
+</body> -->
